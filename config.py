a9825cf505966cd2ba98e88228e117529312f942
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-10-14 11:21:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7711168735:AAHeY83yJGkgQBYl05kH0Hcdb8hCOCda7rU")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "25421165"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "723d180c0fea665460816ab04b6a5840")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Virat:Virat@virat.61nqh.mongodb.net/?retryWrites=true&w=majority&appName=Virat")
